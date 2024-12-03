d398a880c9cd52883a0c356f545d0053286c98c4
Lidia Kovac <lidia.kovac1998@gmail.com>
2024-12-03 22:06:39 +0100

    day 3 - add second star

--- 2024/day 3/index.js
+++ 2024/day 3/index.js
@@ -1,30 +1,36 @@
-/* 
-// .split mul => x,(2,4)
- replaceAll => !(m,u,l,(),num) => mul(2,4) 
-*/
-// const regex = new RegExp(`mul\(\d+,\d+\)/g`, "g")
 import { readFileSync } from "fs"
 import { join } from "path"
 
-// const input = readFileSync(join(import.meta.dirname, "./input.txt"), "utf-8")
+const input = readFileSync(join(import.meta.dirname, "./input.txt"), "utf-8")
 
-const regex = new RegExp(`mul\\(\\d+,\\d+\\)`, "g")
+// const regex = new RegExp(`mul\\(\\d+,\\d+\\)`, "g")
+const regex = new RegExp(`(do\\(\\)|mul\\(\\d+,\\d+\\)|don't\\(\\))`, "g")
 console.log(regex)
-
-// const input = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
-// input.replaceAll(regex, "")
 const res = [...input.matchAll(regex)]
-let sum = 0
-for (const op of res) {
-  //   console.log(op[0])
-  const str = op[0]
 
+const multiplyFromString = (str) => {
   const [first, second] = str
     .split("mul")[1]
     .replace("(", "")
     .replace(")", "")
     .split(",")
-  sum += parseInt(first) * parseInt(second)
-  //   console.log(str.replaceAll(new RegExp(`mul(\\(|\\)/)`, "g"), "")) => TODO
+  console.log(first, second)
+  return parseInt(first) * parseInt(second)
+}
+
+let sum = 0
+let STATUS = "GO"
+for (const op of res) {
+  const str = op[0]
+  console.log(str)
+  if (str.includes("don't")) {
+    STATUS = "STOP"
+  } else if (str.includes("do(")) {
+    STATUS = "GO"
+  } else {
+    if (STATUS === "GO") {
+      sum += multiplyFromString(str)
+    } else continue
+  }
 }
 console.log(sum)
