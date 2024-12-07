245c58c2de418a00c3bc2d17e7ba1278eec57ddd
Lidia Kovac <lidia.kovac1998@gmail.com>
2024-12-07 11:28:43 +0100

    day 6 - part 2 incomplete

--- 2024/day 6/index.js
+++ 2024/day 6/index.js
@@ -6,7 +6,7 @@
 3    ..#.......
 4    .......#..
 5    ..........
-6    .#..^.....
+6    .#.O^.....
 7    ........#.
 8    #.........
 9    ......#...
@@ -23,19 +23,19 @@
 
 import { getMatrixFromString } from "../utils/index.js"
 
-// const input = `....#.....
-// .........#
-// ..........
-// ..#.......
-// .......#..
-// ..........
-// .#..^.....
-// ........#.
-// #.........
-// ......#...`
+const input = `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`
 import { readFileSync, writeFileSync } from "fs"
 import { join } from "path"
-const input = readFileSync(join(import.meta.dirname, "./input.txt"), "utf-8")
+// const input = readFileSync(join(import.meta.dirname, "./input.txt"), "utf-8")
 const guardPattern = new RegExp("\\^|\\>|\\<|v", "g")
 
 const findGuardCoordinates = (mtrx) => {
@@ -69,7 +69,7 @@ const turn90Deg = (char) => {
 }
 
 const findNext = (guard, mtrx, y, x) => {
-  console.log(mtrx[y][x])
+  console.log("Finding next from ", mtrx[y][x])
   switch (guard) {
     case "^":
       if (mtrx[y - 1]) return [y - 1, x]
@@ -89,15 +89,63 @@ const findNext = (guard, mtrx, y, x) => {
   }
 }
 
-const loopMtrx = (mtrx) => {
-  const [gy, gx] = findGuardCoordinates(mtrx)
-  console.log(`Starting at ${gy}, ${gx}`, mtrx.length, mtrx[0].length)
-  let y = gy
-  let x = gx
+const markPath = (mtrx, y, x) => {
+  const [gy, gx, guard] = findGuardCoordinates(mtrx)
+  const [ny, nx] = findNext(guard, mtrx, y, x)
+  console.log("next", mtrx[ny][nx])
+  if (mtrx[ny][nx] === "#") {
+    return "+"
+  } else if (guard === "^" || guard === "v") {
+    return "|"
+  } else if (guard === ">" || guard === "<") {
+    return "-"
+  } else return "X"
+}
+
+const getPrevious = (mtrx, y, x) => {
+  const [gy, gx, guard] = findGuardCoordinates(mtrx)
+  switch (guard) {
+    case "^":
+      if (mtrx[y + 1]) return [y + 1, x]
+      else return [false, false]
+    case ">":
+      if (mtrx[y][x - 1]) return [y, x - 1]
+      else return [false, false]
+    case "v":
+      if (mtrx[y - 1]) return [y - 1, x]
+      else return [false, false]
+    case "<":
+      if (mtrx[y][x + 1]) return [y, x + 1]
+      else return [false, false]
+    default:
+      console.log("Invalid input")
+      return [false, false]
+  }
+}
+
+const loopMtrx = async (mtrx) => {
+  let isLoop = false
+  const [iy, ix, initialGuard] = findGuardCoordinates(mtrx)
+  console.log(`Starting at ${iy}, ${ix}`, mtrx.length, mtrx[0].length)
+  let y = iy
+  let x = ix
   let stop = false
   while (y < mtrx.length) {
     while (x < mtrx[y].length) {
+      await Promise.allSettled([
+        new Promise((res) =>
+          setTimeout(() => {
+            res()
+          }, 500)
+        ),
+      ])
       const [gy, gx, guard] = findGuardCoordinates(mtrx)
+      if (iy === gy && ix === gx && guard === initialGuard) {
+        console.log("Loop")
+        stop = true
+        isLoop = true
+        break
+      }
       if (gy === false || gx === false || guard === false) {
         stop = true
         break
@@ -112,13 +160,22 @@ const loopMtrx = (mtrx) => {
         stop = true
         break
       }
-      if (mtrx[ny][nx] === "#") {
+      const old_guard = mtrx[y][x]
+      if (mtrx[ny][nx] === "#" || mtrx[ny][nx] === "O") {
         console.log("Turning")
         mtrx[gy][gx] = turn90Deg(mtrx[y][x])
+
         continue
       }
-      mtrx[y][x] = "X"
-      if (mtrx[ny][nx] === "." || mtrx[ny][nx] === "X") {
+      mtrx[y][x] = markPath(mtrx, y, x)
+
+      // console.log(mtrx)
+      if (
+        mtrx[ny][nx] === "." ||
+        mtrx[ny][nx] === "|" ||
+        mtrx[ny][nx] === "-" ||
+        mtrx[ny][nx] === "+"
+      ) {
         y = ny
         x = nx
         mtrx[ny][nx] = guard
@@ -127,23 +184,28 @@ const loopMtrx = (mtrx) => {
         join(import.meta.dirname, "./check.txt"),
         mtrx.map((r) => r.join("")).join("\n")
       )
-      // x++
     }
-
     if (stop) break
-    // y++
   }
-
-  //   for (let y = gy; y < mtrx.length; y++) {
-  //     const row = mtrx[y]
-  //     for (let x = gx; x < row.length; x++) {
-  //       const col = row[x]
-  //       cb(mtrx, y, x, ...cbArgs)
-  //     }
-  //   }
+  return isLoop
 }
 
-const matrix = getMatrixFromString(input)
-loopMtrx(matrix)
+let matrix = getMatrixFromString(input)
+const copyOfMatrix = getMatrixFromString(input)
+// // loopMtrx(matrix)
+let loops = 0
+for (let a = 0; a < matrix.length; a++) {
+  const row = matrix[a]
+  for (let b = 0; b < row.length; b++) {
+    const prev = matrix[a][b]
+    // const col = row[b]
+    matrix[a][b] = "O"
+    loops += loopMtrx(matrix) ? 1 : 0
+    matrix[a][b] = prev
+    console.log(matrix)
+  }
+}
+console.log(loops)
+// loopMtrx(matrix)
 const str = matrix.map((r) => r.join("")).join("\n") + "\n"
 console.log([...str.matchAll(new RegExp("X", "g"))].length)
