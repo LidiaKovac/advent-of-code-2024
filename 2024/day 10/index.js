83406db60ee8c9cc4d62bb5b6e5533817b516abe
Lidia Kovac <lidia.kovac1998@gmail.com>
2024-12-10 21:36:52 +0100

    pathfinding is now accurate

--- 2024/day 10/index.js
+++ 2024/day 10/index.js
@@ -7,7 +7,8 @@ import {
   getTop,
 } from "../utils/index.js"
 
-const input = `89010123
+const input = `
+89010123
 78121874
 87430965
 96549874
@@ -15,39 +16,97 @@ const input = `89010123
 32019012
 01329801
 10456732`
-
+const paths = new Set()
 const findPaths = (mtrx, y, x, curr = 1, path = []) => {
   const [top, bottom, left, right] = getDirections(mtrx, y, x)
   if (curr == 1) {
     path.push({ y, x, el: mtrx[y][x] }) //!
-  }
-  if (curr > 9) {
     console.log(parsePathIntoString(path))
+  }
+
+  if (curr > 9) {
+    paths.add(parsePathIntoString(path))
+    console.log(paths)
     return path
   }
+  //path should not be pushed if last num !== 9
+  const possibleRoutes = [
+    { dir: "top", val: top },
+    { dir: "bottom", val: bottom },
+    { dir: "left", val: left },
+    { dir: "right", val: right },
+  ].filter((el) => el.val == curr)
+  if (possibleRoutes.length > 1) {
+    console.log("There are several paths")
+    for (const p of possibleRoutes) {
+      const index = path.findIndex((step) => step.el == curr)
+      if (index > -1) {
+        path.slice(0, index + 1)
+      } else {
+        console.log("Following path", p)
+        //dove sono
+        //le direzioni in cui posso andare
+        //la direzione + val
+        if (p.dir === "top") {
+          console.log("Moving top")
 
-  if (top == curr) {
-    console.log("Found", curr, "on top")
-    path.push({ y: y - 1, x, el: top, dir: "top" })
-    findPaths(mtrx, y - 1, x, curr + 1, path)
-  } else if (bottom == curr) {
-    console.log("Found", curr, "on bottom")
+          path.push({ y: y - 1, x, el: p.val, dir: "top" })
+          console.log(parsePathIntoString(path))
+          findPaths(mtrx, y - 1, x, curr + 1, path)
+        }
+        if (p.dir === "bottom") {
+          console.log("Moving bottom")
 
-    path.push({ y: y + 1, x, el: bottom, dir: "bottom" })
-    findPaths(mtrx, y + 1, x, curr + 1, path)
-  } else if (left == curr) {
-    console.log("Found", curr, "on left")
+          path.push({ y: y + 1, x, el: p.val, dir: "bottom" })
+          console.log(parsePathIntoString(path))
+          findPaths(mtrx, y + 1, x, curr + 1, path)
+        }
+        if (p.dir === "left") {
+          console.log("Moving left")
 
-    path.push({ y, x: x - 1, el: left, dir: "left" })
-    findPaths(mtrx, y, x - 1, curr + 1, path)
-  } else if (right == curr) {
-    console.log("Found", curr, "on right")
+          path.push({ y, x: x - 1, el: p.val, dir: "left" })
+          console.log(parsePathIntoString(path))
+          findPaths(mtrx, y, x - 1, curr + 1, path)
+        }
+        if (p.dir === "right") {
+          console.log("Moving right")
 
-    path.push({ y, x: x + 1, el: right, dir: "right" })
-    findPaths(mtrx, y, x + 1, curr + 1, path)
+          path.push({ y, x: x + 1, el: p.val, dir: "right" })
+          console.log(parsePathIntoString(path))
+          findPaths(mtrx, y, x + 1, curr + 1, path)
+        }
+      }
+    }
+  } else {
+    console.log("Only one path found")
+    if (top == curr) {
+      console.log("Moving top")
+
+      path.push({ y: y - 1, x, el: top, dir: "top" })
+      console.log(parsePathIntoString(path))
+      findPaths(mtrx, y - 1, x, curr + 1, path)
+    } else if (bottom == curr) {
+      console.log("Moving bottom")
+
+      path.push({ y: y + 1, x, el: bottom, dir: "bottom" })
+      console.log(parsePathIntoString(path))
+      findPaths(mtrx, y + 1, x, curr + 1, path)
+    } else if (left == curr) {
+      console.log("Moving left")
+
+      path.push({ y, x: x - 1, el: left, dir: "left" })
+      console.log(parsePathIntoString(path))
+      findPaths(mtrx, y, x - 1, curr + 1, path)
+    } else if (right == curr) {
+      console.log("Moving right")
+
+      path.push({ y, x: x + 1, el: right, dir: "right" })
+      console.log(parsePathIntoString(path))
+      findPaths(mtrx, y, x + 1, curr + 1, path)
+    }
   }
 
-  return path
+  return null
 }
 
 const parsePathIntoString = (arr) => {
@@ -73,6 +132,7 @@ for (let y = 0; y < matrix.length; y++) {
     const col = matrix[y][x]
     if (col === "0") {
       console.log("Calculating paths for ", y, x)
+      const paths = new Set()
       const path = findPaths(matrix, y, x)
     }
   }
