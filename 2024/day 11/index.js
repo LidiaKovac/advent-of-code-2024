21ce2618e1ff01d85b74d25127fc0f99687132ad
Lidia Kovac <lidia.kovac1998@gmail.com>
2024-12-11 22:58:34 +0100

    Day 11 - Second star

--- 2024/day 11/index.js
+++ 2024/day 11/index.js
@@ -6,34 +6,46 @@
 
 */
 
-const input = `125 17`
+const input = `5 62914 65 972 0 805922 6521 1639064`
 
 const arr = input.split(" ")
-const blink = (arr) => {
-    const final = []
-    for (let i = 0; i < arr.length; i++) {
-        const num = arr[i]
+let map = new Map()
+
+for (const num of arr) {
+    map.set(num, (map.get(num) || 0) + 1)
+}
+const blink = () => {
+    const final = new Map()
+
+    for (const [num, count] of map) {
+
+// const num = arr[i]
         if (num == 0) {
-            final[i] = 1
+            // final[i] = 1
+            final.set("1", (final.get("1") || 0) + count)
         } else if (num.length % 2 === 0) {
             let half = parseInt(num.slice(0, num.length / 2))
             let shalf = parseInt(num.slice(num.length / 2))
+            final.set(half.toString(), (final.get(half.toString()) || 0) + count)
+            final.set(shalf.toString(), (final.get(shalf.toString()) || 0) + count)
 
-            final[i] = `${half}/${shalf}`
+            // final[i] = `${half}/${shalf}`
         } else {
-            final[i] = num * 2024
+            const res = num * 2024
+            final.set(res.toString(), (final.get(res.toString()) || 0) + count)
+
         }
     }
-    return final.map(num => num.toString().split("/")).flat()
-}
-let fin = []
-
-for (let i = 0; i < 24; i++) {
-    if (fin.length === 0) fin = blink(arr)
-    fin = blink(fin)
-    console.log(fin)
+    map = final
 }
 
-console.log("FINAL", fin.length)
+for (let i = 0; i < 75; i++) {
+    blink()
+}
+
+console.log("Result:", [...map.values()].reduce((acc, value) => acc + value, 0))
+
+
+// console.log("FINAL", tot)
 
 
